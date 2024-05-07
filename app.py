5226855844732dc52bc3dbb30bd36cdcdbe5c333
Tiwi <141901588+wdyprtiwi@users.noreply.github.com>
2024-05-07 12:00:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 st.header('Hari yang Paling Banyak Menerima Tip')
+
+# reading the database
+url = "https://raw.githubusercontent.com/wdyprtiwi/davis-2024/main/tips.csv"
+data = pd.read_csv(url)
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
 # Bar chart with day against tip
 plt.bar(data['day'], data['tip'])
 
-plt.title("Bar Chart")
+# plt.title("Bar Chart")
 
 # Setting the X and Y labels
 plt.xlabel('Day')
